c18b9e3b436c882e6ce7aa294f7e43f0a490aff8
edgar.sadler <edgar.sadler@CPX-6S1ROXTC39U>
2018-09-10 15:48:16 -0500

    first part of part 1

--- Solution/HelloConsole1/Program.cs
+++ Solution/HelloConsole1/Program.cs
@@ -9,10 +9,17 @@ namespace HelloConsole1
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello NFCU World!");
+
+
+            
+            Console.Write("Type your first name and press Enter");
+            var s = Console.ReadLine();
+            Console.WriteLine("Hello,  " + s + "!");
+
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
