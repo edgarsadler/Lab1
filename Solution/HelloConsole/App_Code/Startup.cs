8c0ab6344a52526935e788d6a19ad678264499f5
edgar.sadler <edgar.sadler@CPX-6S1ROXTC39U>
2018-09-10 11:44:11 -0500

    second try

---
+++ Solution/HelloConsole/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloConsole.Startup))]
+namespace HelloConsole
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Solution/HelloWebForms1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWebForms1.Startup))]
+namespace HelloWebForms1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
