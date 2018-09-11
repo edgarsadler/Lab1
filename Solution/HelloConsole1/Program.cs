72365c42808bbdc7325847600d0641ecf2a1cff5
edgar.sadler <edgar.sadler@CPX-6S1ROXTC39U>
2018-09-11 13:32:23 -0500

    updated this morning

--- Solution/HelloConsole1/Program.cs
+++ Solution/HelloConsole1/Program.cs
@@ -9,20 +9,125 @@ namespace HelloConsole1
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            string nameStr = String.Empty;
+            string ageStr = String.Empty;
+            switch (args.Length)
+            {
 
+                case 0:
+                    {
+                        nameStr = PromptName();
+                        ageStr = PromptAge();
+                        break;
+                    }
+                case 1:
+                    {
+                        if (IsNum(args[0]))
+                        {
+                            nameStr = PromptName();
+                            ageStr = args[0];
+                        }
+                        else
+                        {
+                            nameStr = args[0];
+                            ageStr = PromptAge();
+                        }
 
-            
-            Console.Write("Type your first name and press Enter");
-            var s = Console.ReadLine();
-            Console.WriteLine("Hello,  " + s + "!");
+                        break;
+                    }
+                case 2:
+                    {
+                        if (IsNum(args[0]) && IsNum(args[1]))
+                        {
+                            nameStr = args[0];
+                            ageStr = args[1];
+                        }
+                        else
+                        if (IsNum(args[0]))
+                        {
+                            ageStr = args[0];
+                            nameStr = args[1];
+                        }
+                        else
+                            if (IsNum(args[1]))
+                        {
+                            nameStr = args[0];
+                            ageStr = args[1];
+                        }
+                        else
+                        {
+                            throw new ArgumentException(String.Format("Neiter {0} or {1} are numbers", args[0], args[1]));
+                        }
+                        break;
+                    }
+                default:
+                    throw new ArgumentException(String.Format("Wrong number of Errors"));
+            }
 
+            Console.WriteLine(String.Format("{0} your age is {1}", nameStr, ageStr));
+            // Really your parents Named you a 'Number'?
+            if (IsNum(nameStr))
+            {
+                DoNameIsANumber(nameStr, ageStr);
 
+            }
             Console.ReadKey();
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
+
+        private static bool IsNum(string v)
+        {
+            return int.TryParse(v, out int n);
+
+        }
+
+        private static string PromptAge()
+        {
+            bool ageCaptured = false;
+            string ageStr = string.Empty;
+            do
+            {
+                Console.Write("Enter your Age: ");
+                ageStr = Console.ReadLine();
+                ageCaptured = IsNum(ageStr);
+                if (!ageCaptured)
+                {
+                    Console.WriteLine("Invalid Age");
+                }
+            } while (!ageCaptured);
+            return ageStr;
+        }
+
+        private static string PromptName()
+        {
+            Console.Write("What Is your Name: ");
+            return Console.ReadLine();
+        }
+
+        private static void DoNameIsANumber(string nameStr, string ageStr)
+        {
+            if (nameStr == "99")
+            {
+                Console.WriteLine("I think your Boss Maxwell Smart is Great! I also Have crush on you!");
+                Console.WriteLine("Look at this video https://www.youtube.com/watch?v=I6UQW64hxMI");
+                if (int.Parse(ageStr) > 29)
+                {
+                    Console.WriteLine("You really don't look a day over 29");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your Parents were Mathematicians! ");
+                Console.ReadKey();
+
+
+            }
         }
     }
 }
